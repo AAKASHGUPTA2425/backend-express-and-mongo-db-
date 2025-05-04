1713246592e14a2eeb4c695084b63604c68184c5
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2025-05-04 13:28:42 +0530

    basic connection of backend/frontend

--- Gbasics1/models/Hospital-management/Medical_Recored.models.js
+++
@@ -1,3 +0,0 @@
-import mongoose from 'mongoose'
-const medicalRecoredSchema=new mongoose.Schema({},{timestamps:true})
-export const MedicalRecored=mongoose.model("MedicalRecored",medicalRecoredSchema)
