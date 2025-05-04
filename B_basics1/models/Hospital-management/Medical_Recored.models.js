71b38e6625ea4c9a93dcc4b6e43f63f79192b87e
AAKASHGUPTA2425 <125762441+AAKASHGUPTA2425@users.noreply.github.com>
2025-05-04 13:45:35 +0530

    DataModling

---
+++ B_basics1/models/Hospital-management/Medical_Recored.models.js
@@ -0,0 +1,3 @@
+import mongoose from 'mongoose'
+const medicalRecoredSchema=new mongoose.Schema({},{timestamps:true})
+export const MedicalRecored=mongoose.model("MedicalRecored",medicalRecoredSchema)
